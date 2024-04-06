66149e29f2c3dbb8aeb9eed970d483a14b469e62
Alina Chen <ajcheng7@gmail.com>
2024-04-05 22:20:48 -0500

    transitioning to MVVM

---
+++ roombee/EventViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  EventViewModel.swift
+//  roombee
+//
+//  Created by Alina Chen on 4/3/24.
+//
+//  Manages the state and logic w/ events in the calendar (fetch events from backend, interactions, edits, etc)
+
+import Foundation
---
+++ roombee/NewEventViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  NewEventViewModel.swift
+//  roombee
+//
+//  Created by Alina Chen on 4/3/24.
+//
+// logic for creating a new event (storing form fields, logic to save event, communicate w/ eventviewmodel) 
+
+import Foundation
