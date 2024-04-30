d92e9472e2543e34b02d14c85159828de6adf653
alizenart <97767073+alizenart@users.noreply.github.com>
2024-04-30 00:04:15 -0500

    Change the db events

--- db/createdb.sql
+++ db/createdb.sql
@@ -19,9 +19,9 @@ ALTER TABLE users AUTO_INCREMENT = 80001; -- starting value
 
 CREATE TABLE events
 (
+  event_id int not null,
   user_id int not null, 
   event_title VARCHAR(255),
-  event_date DATE not null, 
   start_time TIME not null, 
   end_time TIME not null, 
   approved BOOL
