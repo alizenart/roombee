4de9dc622397c160822ce3eff30b5bb2472217b2
alizenart <97767073+alizenart@users.noreply.github.com>
2024-11-20 13:24:38 -0600

    Initial Sign In Works!!!
    yay

--- roombee/AuthenticationViewModel.swift
+++ roombee/AuthenticationViewModel.swift
@@ -7,11 +7,14 @@
 
 import Foundation
 import FirebaseAuth
+import FirebaseCore
 import SwiftUI
 import AWSLambda
 import Mixpanel
 import AWSS3
 import AWSCore
+import GoogleSignIn
+import GoogleSignInSwift
 
 
 enum AuthenticationState {
@@ -512,6 +515,49 @@ extension AuthenticationViewModel {
             return false
         }
     }
+    
+}
+
+extension AuthenticationViewModel {
+    func signInWithGoogle() async-> Bool {
+        guard let clientID = FirebaseApp.app()?.options.clientID else{
+            print("No clientID found")
+            return false
+        }
+        
+        let config = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.configuration = config
+        
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+              let window = windowScene.windows.first,
+              let rootViewController = window.rootViewController else{
+            print("There is no root controller")
+            return false
+        }
+        do {
+            let userAuthentication = try await GIDSignIn.sharedInstance.signIn(withPresenting: rootViewController)
+            let user = userAuthentication.user
+            guard let idToken = user.idToken else {
+                print("ID token missing")
+                return false
+            }
+            let accessToken = user.accessToken
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken.tokenString, accessToken: accessToken.tokenString)
+            let result = try await Auth.auth().signIn(with: credential)
+            let firebaseUser = result.user
+            print("User \(firebaseUser.uid) signed in with email \(firebaseUser.email ?? "unknown")")
+            return true
+        }
+        catch {
+            print(error.localizedDescription)
+            errorMessage = error.localizedDescription
+            return false
+        }
+        
+        
+        return false
+    }
+    
 }
 
 
